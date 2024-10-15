d8fa1f3d324a382c9cd72450f24413ea0a124380
James Jackson <jjackson0228@gmail.com>
2024-10-15 17:06:33 -0400

    added a theme in script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 export default {
-  mode: 'jit', // Enable JIT mode
+  // mode: 'jit', // Enable JIT mode
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
