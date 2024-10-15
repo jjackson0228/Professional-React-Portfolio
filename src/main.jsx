c76f4c7e73e0df1eed8fbf77f0c56d6e71151136
James Jackson <jjackson0228@gmail.com>
2024-10-15 14:22:09 -0400

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDom from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Home from './pages/Home';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/', //root/home route
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true, // index true, When first visited app home is the first page rendered inside
+        element: <Home />, //home will be first component rendered
+      },
+    ],
+  },
+]);
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
