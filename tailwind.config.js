41d12d96a500ea251cc123bc02284f0cdcc53289
James Jackson <jjackson0228@gmail.com>
2024-10-15 14:40:52 -0400

    added tailwind and react-router-dom as dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+  mode: 'jit', // Enable JIT mode
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
